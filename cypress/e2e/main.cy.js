490653d1b01ad28984f6714f6860e8c356863af7
Aluvas <black.samlh@gmail.com>
2023-03-08 14:55:38 -0800

    more tests

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -1,23 +1,48 @@
-import mainPage from "../pages/mainPage"
+import mainPage from '../pages/mainPage'
 
-const omdb = new mainPage();
+const homePage = new mainPage();
 
-describe("OMDb Technical Tests", () => {
+describe('OMDb Technical Tests', () => {
     //using beforeEach instead of before because it will navigate out of the shadow DOM after each individual command. Re-navigating is not ideal-this is to be fixed.
     beforeEach(() => {
-        omdb.visit();
+        homePage.visit();
     })
 
-    it("Complete a search", () => {
-        omdb.search("crow");
+    it('Complete a search', () => {
+        homePage.getMovie('crow');
     })
 
-    it("Test Autocomplete Timing", () => {
-        omdb.autocomplete("velc");
+    it('Test Autocomplete Timing', () => {
+          let started          
+        cy.get('input#search').type('velc')
+        .then(() => {
+            started = +new Date();
+        });
+        cy.get('[class="movie-list-item"]').should('be.visible')
+        .then(() => {
+            const finished = +new Date()
+            const elapsed = finished-started
+            expect(elapsed, 'cards appear in less than 300ms').to.be.lessThan(301)
+        });        
     })
     
-    it("Validate Card Contents", () => {
-        omdb.autocomplete("hat");
-        //then check "movie-list-item" to make sure "movie-image", "movie-title", and "movie-info" are populated
+    it('Validate Card Contents', () => {
+        homePage.getMovie('hat');
+        homePage.cardImg();
+        homePage.cardYear();
+        homePage.cardYearText();
+        homePage.cardTitle();
+    })
+
+    it('Validate Card Image Size', () => {
+        homePage.getMovie('fun');
+        cy.get('movie-image').eq(0)
+        .find('img')
+        .invoke('css', 'width')
+        .then(str => parseInt(str)).should('eq', 40)
+    })
+
+    it('Validate Search Bar Highlight', () => {
+        cy.get('input#search').click();
     })
 })
--- cypress/pages/mainPage.js
+++ cypress/pages/mainPage.js
@@ -2,23 +2,39 @@ class mainPage {
     visit() {
         cy.visit("/")
     }
-    search(text) {
+    getMovie(text) {
         cy.get('input#search').type(text);
-        cy.get('[class="movie-list-item"]:nth(0)').should('be.visible')
+        return cy.get('[class="movie-list-item"]').eq(0).should('be.visible');
     }
-    autocomplete(text) {
-        let started          
-        cy.get('input#search').type(text)
-        .then(() => {
-            started = +new Date()
-        });
-        cy.get('[class="movie-list-item"]:nth(0)').should('be.visible')
-        .then(() => {
-            const finished = +new Date()
-            const elapsed = finished-started
-            expect(elapsed, 'cards appear in less than 300ms').to.be.lessThan(301)
-        });        
+    cardImg() {
+        return cy.get('movie-image').eq(0)
+            .find('img')
+            .should('be.visible')
+            .and('have.class', 'movie-img')
     }
-    
+    cardTitle() {
+        return cy.get('movie-title').eq(0)
+            .find('h3')
+            .should('be.visible')
+            .and('have.class', 'movie-title')
+    }
+    cardYear() {
+        return cy.get('movie-info').eq(0)
+            .find('div')
+            .find('h2')
+            .should('be.visible')
+            .and('have.class', 'movie-year')
+    }
+    cardYearText() {
+        return cy.get('movie-info').eq(0)
+            .find('div')
+            .find('h5')
+            .should('be.visible')
+            .and('have.class', 'movie-year-text')
+    }  
+    openCard() {
+        cy.get('movie-list-item').eq(0)
+        .find('.movie-list-item').should('be.visible').click()
+    }
 }
 module.exports = mainPage;
