66ddf6c056280f33366ab83208a6a498afcb20e0
Aluvas <black.samlh@gmail.com>
2023-03-14 13:08:47 -0700

    Final code lock

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -1,55 +1,59 @@
 import mainPage from '../pages/mainPage'
 
-const homepage = new mainPage();
+const homepage = new mainPage()
 
 describe('OMDb Technical Tests', () => {
 
   beforeEach(() => {
-    homepage.visit();
+    homepage.visit()
   })
 
-  it.only('Complete a search', () => {
-    homepage.populateList('crow', 0);
+  it('Complete a search', () => {
+    homepage.searchBar('fun').then(() => {
+      homepage.checkList(0)
+    })
   })
 
   it('Test Autocomplete Timing', () => {
     let started
-    homepage.searchBar().type('velc').then(() => {
-      started = +new Date();
-    });
+    homepage.searchBar('velc').then(() => {
+      started = +new Date()
+    })
     cy.get('[class="movie-list-item"]').should('be.visible')
       .then(() => {
         const finished = +new Date()
         const elapsed = finished - started
         expect(elapsed, 'cards appear in less than 300ms').to.be.lessThan(301)
-      });
+      })
   })
 
   it('Validate Card Contents', () => {
-    homepage.populateList('hat', 0);
-    homepage.cardImg(0);
-    homepage.cardYear(0);
-    homepage.cardYearText(0);
-    homepage.cardTitle(0);
+    homepage.searchBar('hat').then(() => {
+      homepage.checkList(0)
+      homepage.cardImg(0)
+      homepage.cardYear(0)
+      homepage.cardYearText(0)
+      homepage.cardTitle(0)
+    })
   })
 
   it('Validate Card Image Size', () => {
     let width
-    homepage.populateList('fun', 0);
+    homepage.searchBar('fun')
+    homepage.checkList(0)
     homepage.cardImg(0).then(($val) => {
       width = $val.css('width')
-    });
-    homepage.cardImg(1).then((img) => {
-      cy.get(img).invoke('css', 'width').should('eq', width)
+      homepage.cardImg(1).invoke('css', 'width').should('eq', width)
     })
+
   })
 
   it('Validate Search Bar Highlight', () => {
-    homepage.searchBar().then(($val) => {
+    cy.get('[type="text"]').then(($val) => {
       const outline = $val.css('outline')
       expect(outline).to.include('none 0px')
-      homepage.searchBar().click()
-      homepage.searchBar().then(($val2) => {
+      homepage.searchBar('walk').click()
+      homepage.searchBar('s').then(($val2) => {
         expect($val2.css('outline')).not.to.eq(outline)
         expect($val2.css('outline')).to.include('auto 1px')
       })
@@ -58,26 +62,30 @@ describe('OMDb Technical Tests', () => {
 
   // //As per the spec, this test does not function, but this is how it would be written if it were to function.
   it.skip('Validate Movie Page Displays Correct Content', () => {
-    homepage.openCard('shark', 0);
-    homepage.moviePageImage(0);
+    homepage.searchBar('crow').then(() => {
+      homepage.checkList(0).click()
+      homepage.moviePageImage(0)
+    })
   })
 
-  it('Paste Movie Title', () => {
-    cy.get('[type="text"]').click()
-    cy.realType('pants')
-    cy.realPress(['Control', 'a', 'Control', 'x'])
-    cy.realType('shoes')
-    cy.realPress(['Control', 'a', 'Control', 'v'])
-    homepage.cardImg(0);
-    homepage.cardYear(0);
-    homepage.cardYearText(0);
-    homepage.cardTitle(0);
+
+  it.only('Paste Movie Title', () => {
+    const textToPaste = 'pants'
+    homepage.searchBar(textToPaste).then(() => {
+      cy.realPress(['Control', 'a', 'Control', 'x'])
+      cy.realType('shoes')
+      cy.realPress(['Control', 'a', 'Control', 'v'])
+      homepage.cardTitle(0).contains(textToPaste, { matchCase: false })
+    })
   })
 
   it('Mock Movie Data', () => {
     cy.fixture('exampleMovie').then((data) => {
-      homepage.populateList('Hat', 0)
-      homepage.cardTitle(0).invoke('text', data.movieTitle)
+      homepage.searchBar('cat')
+      homepage.checkList(0)
+      homepage.cardTitle(0).invoke('text', data.movieTitle).then(() => {
+        homepage.cardTitle(0).invoke('text').should('eq', 'Flow The Movie')
+      })
     })
   })
 })
--- cypress/pages/mainPage.js
+++ cypress/pages/mainPage.js
@@ -1,13 +1,12 @@
 class mainPage {
     visit() {
-        cy.visit("/")
+        cy.visit('/')
     }
-    searchBar() {
-       return cy.get('[type="text"]')
+    searchBar(text) {
+        return cy.get('[type="text"]').type(text)
     }
-    populateList(text, i) {
-        cy.get('[type="text"]').type(text);
-        return cy.get('[class="movie-list-item"]').eq(i).should('be.visible');
+    checkList(i) {
+        return cy.get('[class="movie-list-item"]').eq(i).should('be.visible')
     }
     cardImg(i) {
         return cy.get('movie-image').eq(i)
@@ -32,17 +31,12 @@ class mainPage {
             .find('h5')
             .should('be.visible')
             .and('have.class', 'movie-year-text')
-    }  
-    openCard(text, i) {
-        cy.get('[type="text"]').type(text);
-        cy.get('[class="movie-list-item"]').eq(i).click();
     }
     moviePageImage(i) {
         return cy.get('movie-card').eq(i)
-        .find('img')
-        .should('be.visible')
-        .and('have.attr', 'src')
+            .find('img')
+            .should('be.visible')
+            .and('have.attr', 'src')
     }
-
 }
-module.exports = mainPage;
+module.exports = mainPage
